7c165a11b7b570b15768225168b46ba4a4a4c867
Adrien Chaix <chaix.adrien@laposte.net>
2021-09-29 19:15:58 +0200

    Working page

--- components/App.vue
+++ components/App.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="gantt-container">
-    <Gantt
-      :view-mode="mode"
-      :tasks="tasks"
-      @task-date-updated="updateTaskDate($event)"
-      @task-progress-updated="updateTaskProgress($event)"
-    />
-    <button @click="addRandomTask">Add</button>
+  <div class="d-flex flex-row flex-wrap justify-space-around">
+    <v-card class="container-gantt">
+      <div>
+        <ViewModeButton
+          @click="changeViewMode"
+          handle="Year"
+          :viewMode="mode"
+          title="Année"
+        />
+        <ViewModeButton
+          @click="changeViewMode"
+          handle="Month"
+          :viewMode="mode"
+          title="Mois"
+        />
+        <ViewModeButton
+          @click="changeViewMode"
+          handle="Week"
+          :viewMode="mode"
+          title="Semaine"
+        />
+        <ViewModeButton
+          @click="changeViewMode"
+          handle="Day"
+          :viewMode="mode"
+          title="Journée"
+        />
+      </div>
+      <Gantt
+        ref="gantt"
+        :view-mode="mode"
+        :tasks="tasks"
+        @task-date-updated="updateTaskDate($event)"
+        @task-progress-updated="updateTaskProgress($event)"
+      />
+    </v-card>
+    <v-card class="tasks-container" elevation="2">
+      <h1 style="color: white">Mes Taches</h1>
+      <Task
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        @onTaskChange="(newT) => updateTask(task, newT)"
+      />
+      <NewTask @onNewTask="(newT) => addTask(newT)" :tasks="tasks" />
+    </v-card>
   </div>
 </template>
 
@@ -14,25 +52,26 @@
 import "../assets/style.css";
 import Vue from "vue";
 import { Task } from "frappe-gantt";
+import moment from "moment";
 
 export default Vue.extend({
   data() {
     const tasks: Task[] = [
       {
-        id: "1",
+        id: "0",
         name: "Hello",
-        start: "2021-04-05",
-        end: "2021-04-07",
+        start: moment().add(-3, "d").format("YYYY-MM-DD"),
+        end: moment().add(-2, "d").format("YYYY-MM-DD"),
         progress: 10,
         dependencies: "",
       },
       {
-        id: "2",
+        id: "1",
         name: "World",
-        start: "2021-04-10",
-        end: "2021-04-13",
+        start: moment().format("YYYY-MM-DD"),
+        end: moment().add(2, "d").format("YYYY-MM-DD"),
         progress: 20,
-        dependencies: "1",
+        dependencies: "0",
       },
     ];
     return {
@@ -41,17 +80,6 @@ export default Vue.extend({
     };
   },
   methods: {
-    addRandomTask() {
-      const id = Math.random();
-      this.tasks.push({
-        id: id.toString(),
-        name: id.toString(),
-        start: "2021-01-01",
-        end: "2021-01-05",
-        dependencies: "0",
-        progress: Math.random() * 100,
-      });
-    },
     updateTaskDate(event: any) {
       const { task } = event;
       task.end = event.end;
@@ -61,8 +89,19 @@ export default Vue.extend({
       const { task } = event;
       task.progress = event.progress;
     },
-    demoViewMode(viewMode: string) {
-      this.mode = viewMode;
+    updateTask(oldTask: Task, newTask: Task) {
+      console.log("update", oldTask, newTask);
+      oldTask.name = newTask.name;
+      oldTask.start = newTask.start;
+      oldTask.end = newTask.end;
+      if (this.$refs.gantt) (this.$refs.gantt as any).updateTasks();
+    },
+    changeViewMode(mode: string) {
+      console.log("change view mode", mode);
+      this.mode = mode;
+    },
+    addTask(task: Task) {
+      this.tasks.push(task);
     },
   },
 });

5f78b8ac8b158661fa4b6566c9d4c1d5e15790cb
Adrien Chaix <chaix.adrien@laposte.net>
2021-09-29 19:15:02 +0200

    View Mode Button

---
+++ components/ViewModeButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-btn
+    :color="viewMode === handle ? 'accent' : undefined"
+    elevation="2"
+    small
+    x-small
+    v-on:click="onClick"
+    >{{ title }}</v-btn
+  >
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    handle: {
+      type: String,
+      required: true,
+    },
+    viewMode: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click", this.handle);
+    },
+  },
+});
+</script>
